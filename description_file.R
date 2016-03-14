b89ee0f245492aa9cb84105cd23ba1e128f73374
Yaqi Zhou <zhou277@wisc.edu>
2016-03-14 15:54:50 -0500

    Updated code for producing description file; modify colnames of the data set

--- description_file.R
+++ description_file.R
@@ -1,10 +1,15 @@
 library(dplyr)
-data <- read.csv("unifiedMLDataMulti.csv",header=TRUE)
-names(data)
-length(data)
+data <- read.csv("unifiedMLDataMulti.csv", header=TRUE)
+name <- names(data)
+colnames(data) <- gsub("_", ".", name)
+fname <- "MLData.csv"
+write.csv(data, fname, row.names=FALSE)
+
+dat <- read.csv(fname, header=TRUE)
+length(dat)
 type <- c("b","b","b","d","b","n","b","b","b")
-des <- data.frame(V1=as.character(1:length(data)),V2=names(data),V3=type)
-filename <- data.frame(V1=c("unifiedMLDataMulti.csv","?","2"), V2=rep("NA",3),V3=rep("NA",3))
+des <- data.frame(V1=as.character(1:length(dat)),V2=names(dat),V3=type)
+filename <- data.frame(V1=c(fname,"NA","2"), V2=rep("NA",3),V3=rep("NA",3))
 des_file <- full_join(filename,des)
 write.table(des_file,"description_file.txt",row.names=FALSE, col.names=FALSE,quote=FALSE)
   
