e4ea266f7a6a40fa6f77c29f75e9c1b878860df2
Yaqi Zhou <zhou277@wisc.edu>
2016-03-14 15:21:14 -0500

    description file
    codes for producing description file

---
+++ description_file.R
@@ -0,0 +1,10 @@
+library(dplyr)
+data <- read.csv("unifiedMLDataMulti.csv",header=TRUE)
+names(data)
+length(data)
+type <- c("b","b","b","d","b","n","b","b","b")
+des <- data.frame(V1=as.character(1:length(data)),V2=names(data),V3=type)
+filename <- data.frame(V1=c("unifiedMLDataMulti.csv","?","2"), V2=rep("NA",3),V3=rep("NA",3))
+des_file <- full_join(filename,des)
+write.table(des_file,"description_file.txt",row.names=FALSE, col.names=FALSE,quote=FALSE)
+  
