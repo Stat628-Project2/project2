b176da19caf8dededbdaecfab41fb241a2214068
Kejia (KJ) Shi <kejia.shi@hotmail.com>
2016-03-28 17:03:35 -0500

    Create smallClean.R
    Cleaning code on the 100k dataset on CHTC

---
+++ CHTC/smallClean.R
@@ -0,0 +1,4 @@
+arguments = smallClean.R
+requirements = (OpSys == "LINUX") && (OpSysMajorVer == 6)
+
+source('http://raw.githubusercontent.com/JustinChu/STAT545A_MovieStats/master/cleanMovieLensData.R')
