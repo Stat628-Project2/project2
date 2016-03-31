f351c9b5acc14461fc9baf5e7f700a63d338b2b7
Kejia (KJ) Shi <kejia.shi@hotmail.com>
2016-03-31 10:11:03 -0500

    updated for a newer version

--- CHTC/smallClean.R
+++
@@ -1,4 +0,0 @@
-arguments = smallClean.R
-requirements = (OpSys == "LINUX") && (OpSysMajorVer == 6)
-
-source('http://raw.githubusercontent.com/JustinChu/STAT545A_MovieStats/master/cleanMovieLensData.R')
