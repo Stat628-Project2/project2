6c3c4a385353ad06cd48555fedfb16f453912a8b
Kejia (KJ) Shi <kejia.shi@hotmail.com>
2016-04-07 01:01:57 -0500

    Update 20m-clean.R

--- 20m-clean.R
+++ 20m-clean.R
@@ -3,6 +3,7 @@ library(dplyr)
 
 #Author: Kejia Shi
 #Note: This script creates a giant rating matrix for specific given data subset of users and movies.
+#(Apr 6) V2.0 Update: shorten steps creating rating matrix significantly
 
 #Remove previous data space
 rm(list=ls())
@@ -15,43 +16,15 @@ dataTbl <- read.csv(file="ml-20m/reduced.csv", header = TRUE, sep = ",")
 itemTbl <- read.csv(file="ml-20m/movies.csv", header = TRUE, sep = ",")
 dataTbl <- subset(dataTbl, select = -c(timestamp))
 
-userNum <- dataTbl[nrow(dataTbl), 1]
-itemNum <- nrow(itemTbl)
+ratings_ori <- dataTbl[complete.cases(dataTbl), ]
+full <- setNames(with(ratings_ori, expand.grid(sort(unique(userId)), sort(unique(movieId)))),c("userId", "movieId"))
+ratings_aug <- merge(full, ratings_ori, all = TRUE)
+ratings <- matrix(ratings_aug$rating, nrow = length(unique(ratings_aug$userId)), byrow = TRUE)
 
-#Create itemId instead of using movingID
-itemId <- as.numeric(rownames(itemTbl))
-itemTbl <- cbind(itemId,itemTbl)
-##add such column in rating table as well
-itemTbl_temp <- itemTbl[,-c(3,4)]
-dataTbl <- left_join(dataTbl, itemTbl_temp, by = "movieId")
+rownames(ratings) <- unique(ratings_aug$userId)
+colId <- unique(ratings_aug$movieId)
+tmp <- data.frame(movieId=colId, flag=1)
+tmp <- inner_join(itemTbl, tmp, by = "movieId", copy = TRUE)
+colnames(ratings) <- tmp$title
 
-#create augmented rating table
-ratings_aug <- matrix(NA, nrow = userNum * itemNum, ncol = 3)
-colnames(ratings_aug) <- c("userId", "itemId", "user.item")
-ratings_aug[,2] <- row(ratings_aug)[,2] %% itemNum
-ratings_aug[,1] <- (row(ratings_aug)[,1]-1) %/% itemNum + 1
-ratings_aug[,3] <- row(ratings_aug)[,3]
-dataTbl <- dataTbl %>% mutate(user.item=(userId-1)*itemNum+itemId)
-ratings_aug <- data.frame(ratings_aug)
-ratings_aug <- left_join(ratings_aug, dataTbl, by="user.item")
-ratings_aug <- ratings_aug[, -c(3,4,5,7)]
-colnames(ratings_aug) <- c("userId", "itemId", "rating")
-
-#Build a rating matrix
-##setup an empty matrix
-ratings <- matrix(NA, nrow = userNum, ncol = itemNum)
-
-#rating <- ratings_aug$rating
-
-
-colnames(ratings) <- col(ratings)[1,]
-rownames(ratings) <- row(ratings)[,1]
-##copying ratings
-ratings[,] <- ratings_aug$rating[(row(ratings)-1)*itemNum+dataTbl$itemId]
-#title <- itemTbl$title
-#colnames(ratings) <- title
-
-#ratings[( as.numeric(rownames(ratings)) -1 ) * itemNum + as.numeric(colnames(ratings))] <- ratings_aug$rating[( as.numeric(rownames(ratings)) -1 ) * itemNum + as.numeric(colnames(ratings))]
-#ratings[as.numeric(rownames(ratings)-1) * itemNum + as.numeric(colnames(ratings))] <- ratings_aug$rating
-ratings[ratings_aug$userId, ratings_aug$itemId] <- ratings_aug$rating[(ratings_aug$userId-1)*itemNum+ratings_aug$itemId]
-
+affinity.matrix<- as(ratings,"realRatingMatrix")
